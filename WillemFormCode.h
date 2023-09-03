cc417ad77515e161d5f1a5942921e264c5432ee6
Willem65 <Willempost77@gmail.com>
2023-09-03 03:50:29 +0200

    1e commit

---
+++ WillemFormCode.h
@@ -0,0 +1,130 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef WillemFormCodeH
+#define WillemFormCodeH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <ComCtrls.hpp>
+
+
+
+//---------------------------------------------------------------------------
+class TWillemForm1 : public TForm
+{
+
+__published:	// IDE-managed Components
+        TMemo *memLog;
+        TEdit *UDPEdit;
+        TButton *btnOpen;
+        TTimer *RefreshTimer;
+        TComboBox *lstInterfaces;
+        TLabel *Label2;
+        TLabel *Label22;
+        TProgressBar *ProgressBar1;
+    TButton *Button1;
+	TLabel *Label1;
+	TLabel *Label3;
+	TTrackBar *TrackBar1;
+	TProgressBar *ProgressBar2;
+	TProgressBar *ProgressBar3;
+	TProgressBar *ProgressBar4;
+	TTrackBar *TrackBar2;
+	TLabel *Label4;
+	TLabel *Label5;
+	TButton *Button2;
+	TLabel *Label6;
+	TButton *Button3;
+	TLabel *Label7;
+	TButton *Button4;
+	TButton *Button5;
+	TButton *Button6;
+	TButton *Button7;
+	TButton *Button8;
+	TButton *Button9;
+	TButton *Button10;
+	TButton *Button11;
+	TButton *Button12;
+	TButton *Button13;
+	TButton *Button14;
+	TButton *Button15;
+	TButton *Button16;
+	TButton *Button17;
+	TButton *Button18;
+	TButton *Button19;
+	TButton *Button20;
+	TButton *Button21;
+	TButton *Button22;
+	TButton *Button23;
+	TButton *Button24;
+	TLabel *Label8;
+	TLabel *Label9;
+	TLabel *Label10;
+	TLabel *Label11;
+	TLabel *Label12;
+	TLabel *Label13;
+	TLabel *Label14;
+	TLabel *Label15;
+	TLabel *Label16;
+	TLabel *Label17;
+	TLabel *Label18;
+	TLabel *Label19;
+	TGroupBox *GroupBox1;
+	TGroupBox *GroupBox2;
+	TGroupBox *GroupBox3;
+	TGroupBox *GroupBox4;
+	TTrackBar *TrackBar3;
+	TTrackBar *TrackBar4;
+	TTrackBar *TrackBar5;
+	TTrackBar *TrackBar6;
+	TLabel *Label20;
+	TLabel *Label21;
+	TLabel *Label23;
+	TLabel *Label24;
+	TLabel *Label25;
+	TLabel *Label26;
+	TMemo *Memo1;
+	TLabel *Label27;
+	TLabel *Label28;
+	TLabel *Label29;
+	TLabel *Label30;
+	TLabel *Label31;
+	TProgressBar *ProgressBar5;
+	TProgressBar *ProgressBar6;
+	TLabel *Label32;
+	TLabel *Label33;
+	TLabel *Label34;
+	TProgressBar *ProgressBar7;
+	TLabel *Label35;
+	TProgressBar *ProgressBar8;
+	TProgressBar *ProgressBar9;
+	TProgressBar *ProgressBar10;
+	TLabel *Label37;
+	TProgressBar *ProgressBar11;
+	TProgressBar *ProgressBar12;
+	TProgressBar *ProgressBar13;
+	TProgressBar *ProgressBar14;
+	TProgressBar *ProgressBar15;
+	TProgressBar *ProgressBar16;
+	TLabel *Label36;
+	TLabel *Label38;
+	TLabel *Label39;
+	TLabel *Label40;
+	TLabel *Label41;
+        void __fastcall FormCreate(TObject *Sender);
+        void __fastcall btnOpenClick(TObject *Sender);
+        void __fastcall RefreshTimerTimer(TObject *Sender);
+    void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+         struct mbn_handler *mbn;
+         struct mbn_if_ethernet *iflist;
+        __fastcall TWillemForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TWillemForm1 *WillemForm1;
+//---------------------------------------------------------------------------
+#endif
---
+++ __astcache/c@@users@willem@documents@embarcadero@studio@projects@mambanet-tester-werkend-houden/willemformcode.cpp
@@ -0,0 +1,420 @@
+﻿//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#define MBN_VARARG
+
+#include <stdio.h>
+//#include <string>
+//#include <sstream>
+//#include <stdarg.h>
+//#include <string.h>
+ 
+
+#include "mbn.h" 
+#include "WillemFormCode.h"
+
+
+
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+
+
+#define LF_APP 1
+#define LF_CB  2
+#define LF_OBJ 3
+#define LF_ADR 4
+
+TWillemForm1 *WillemForm1;
+
+static int online = 0;
+
+
+
+
+
+//  Callback functies
+void mOnlineStatus(struct mbn_handler *mbn, unsigned long addr, char valid)
+{
+	if( valid )
+		online = (int)valid;
+
+}
+
+char VuValue, vuValueR;
+char bufferVuMeter[16];
+
+int mSensorDataChanged(struct mbn_handler *mbn, struct mbn_message *msg, unsigned short obj, unsigned char type, union mbn_data dat)
+{
+
+
+	if ( obj == 1088 )
+	{
+		VuValue = dat.UInt;
+		//sprintf(bufferVuMeter, "#%04d_%04d", obj, dat.UInt);
+		bufferVuMeter[0]=obj;
+		bufferVuMeter[1]=VuValue;
+	}
+
+	else if ( obj == 1089 )
+	{
+		vuValueR = dat.UInt;
+	}
+
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+__fastcall TWillemForm1::TWillemForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+	iflist = NULL;
+}
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TWillemForm1::FormCreate(TObject *Sender)
+{
+	char err[MBN_ERRSIZE], ifdesc[512];
+	struct mbn_if_ethernet *n;
+	
+    if((iflist = mbnEthernetIFList(err)) == NULL)
+	{
+        //addlog(LF_APP, "Error getting interface list: %s", err);
+		UDPEdit->Text = "Willem";
+	}
+    else
+	{
+        lstInterfaces->Items->Clear();
+		
+        for(n=iflist; n!=NULL; n=n->next)
+        {
+            sprintf(ifdesc, "%02X:%02X:%02X:%02X:%02X:%02X", n->addr[0], n->addr[1], n->addr[2], n->addr[3], n->addr[4], n->addr[5]);
+            lstInterfaces->Items->AddObject(ifdesc, (TObject *)n->name);
+        }
+
+        lstInterfaces->ItemIndex = 0;
+
+	}
+
+	UDPEdit->Text = "192.168.1.76";
+
+    memLog->Text = online;
+
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TWillemForm1::btnOpenClick(TObject *Sender)
+{
+
+
+
+	int cnt;
+	struct mbn_interface *itf;
+	char error[MBN_ERRSIZE];
+
+	char Temp[256];
+	//char *url;
+	//char *port_found;
+	char url[256];
+	char port[16];
+	char obj_desc[32];
+	struct mbn_node_info thisnodeB;
+	int cntSwitch;
+	struct mbn_object objects[350];
+	int cntObject = 0;
+
+
+ 
+	String udpText = UDPEdit->Text;
+
+	//AnsiString udpText = UDPEdit->Text;
+
+	//UnicodeString udpText = UDPEdit->Text;
+
+	StrPCopy(Temp, udpText);
+
+
+   itf = mbnUDPOpen(Temp, strdup("34848"), NULL, error);  // Maak verbinding met ethernet  // strdup( ) handige functie
+
+
+
+	if( itf == NULL )
+	{
+		memLog->Lines->Add( "EthernetOpen failed: %s" );
+		return;
+	}
+
+	//thisnodeB.UniqueIDPerProduct = 8;
+
+
+	thisnodeB.MambaNetAddr = 0;
+	thisnodeB.Services = 0;
+	sprintf(thisnodeB.Description, "Axite Control Surface 6 Faders + CRM");
+	sprintf(thisnodeB.Name, "Test Node D&R");
+	thisnodeB.ManufacturerID = 0x0001;
+	thisnodeB.ProductID = 0x0022;
+	thisnodeB.UniqueIDPerProduct = 0x106;   //UniqueIDPerProduct
+	thisnodeB.HardwareMajorRevision = 0;
+	thisnodeB.HardwareMinorRevision = 0;
+	thisnodeB.FirmwareMajorRevision = 1;
+	thisnodeB.FirmwareMinorRevision = 3;
+	thisnodeB.FPGAFirmwareMajorRevision = 0;
+	thisnodeB.FPGAFirmwareMinorRevision = 0;
+	thisnodeB.NumberOfObjects = 0;
+	thisnodeB.DefaultEngineAddr = 0;
+	thisnodeB.HardwareParent[0] = 0;
+	thisnodeB.HardwareParent[1] = 0;
+	thisnodeB.HardwareParent[2] = 0;
+	thisnodeB.ServiceRequest = 0;
+	/* 1024 - 1035: OLED Display lines */
+	for (cnt=0; cnt<6; cnt++)
+	{
+		sprintf(obj_desc, "Display %d Line 1", cnt+1);
+		
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_OCTETS, 8, 0, 127, 0, "");
+		
+		//objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_UINT, 0, 2 , 0, 512, 256, MBN_DATATYPE_NODATA);
+	}
+	for (cnt=0; cnt<6; cnt++) 
+	{
+		sprintf(obj_desc, "Display %d Line 2", cnt+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_OCTETS, 8, 0, 127, 0, "");
+	}
+	
+	/* 1036 - 1047: Stereo Audio level metering */
+	for (cnt=0; cnt<6; cnt++) 
+	{
+		sprintf(obj_desc, "Display Meter %d Left dB", cnt+1);
+		objects[cntObject++] = MBN_OBJ( obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_FLOAT, 2, -50.0, 5.0, -50.0, -50.0);
+		sprintf(obj_desc, "Display Meter %d Right dB", cnt+1);
+		objects[cntObject++] = MBN_OBJ( obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_FLOAT, 2, -50.0, 5.0, -50.0, -50.0);
+	}
+	
+	/* 1048 - 1053: Encoder rotation */
+	for (cnt=0; cnt<6; cnt++) 
+	{
+		sprintf(obj_desc, "Encoder %d", cnt+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_SINT, 1, 1, -128, 127, 0, MBN_DATATYPE_NODATA);
+	}
+	
+	/* 1054 - 1059: Encoder switch */
+	for (cnt=0; cnt<6; cnt++) 
+	{
+		sprintf(obj_desc, "Encoder %d Switch", cnt+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_STATE, 1, 1, 0, 1, 0, MBN_DATATYPE_NODATA);
+	}
+	
+	
+	
+	/* 1060 - 1083: Module switches (TB, INP B, CUE/PFL, START/STOP) */
+	for (cntSwitch=0; cntSwitch<4; cntSwitch++)
+	{
+		for (cnt=0; cnt<6; cnt++) 
+		{
+			sprintf(obj_desc, "Switch %d.%d", cnt+1, cntSwitch+1);
+			objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_STATE, 1, 1, 0, 1, 0, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+		}	
+	}
+	
+	/* 1084 - 1089: Fader (also actuator[A] to store actual value set by other node) */
+	for (cnt=0; cnt<6; cnt++) 
+	{
+		sprintf(obj_desc, "Fader %d", cnt+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_UINT, 1, 2, 0, 1023, 0, MBN_DATATYPE_UINT, 2, 0, 1023, 0, 0);
+	}
+	
+	/* 1090 - 1113: Module switches ON color */
+	for (cntSwitch=0; cntSwitch<4; cntSwitch++) 
+	{
+		for (cnt=0; cnt<6; cnt++) 
+		{
+			sprintf(obj_desc, "Switch %d.%d on color", cnt+1, cntSwitch+1);
+			objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 3, 2, 2);
+		}
+	}
+	
+	/* 1114 - 1137: Module switches OFF color */
+	for (cntSwitch=0; cntSwitch<4; cntSwitch++) 
+	{ /*cntSwitch == switchID */
+		for (cnt=0; cnt<6; cnt++)  /* cnt == module */
+		{
+			sprintf(obj_desc, "Switch %d.%d off color", cnt+1, cntSwitch+1);
+			objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 3, 0, 0);
+		}
+	}
+	
+	/* 1138 - 1143: PGM on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) 
+	{
+		sprintf(obj_desc, "PGM %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	/* 1144 - 1149: SUB on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "SUB %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1150 - 1155: EQ on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "EQ %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1156 - 1161: DYN on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Dynamics %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1162 - 1167: AUX1 on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Aux1 %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1168 - 1173: AUX2 on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Aux2 %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1174 - 1179: AUX3 on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Aux3 %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1180 - 1185: AUX4 on/off */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Aux4 %d on/off", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	
+	/* 1186 - 1191: Peak Indicator (ppm) */
+	for (cntSwitch=0; cntSwitch<6; cntSwitch++) {
+		sprintf(obj_desc, "Peak %d", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}  
+	
+	/* 1192 - 1240: CRM Switches */
+	for (cntSwitch=0; cntSwitch<49; cntSwitch++) 
+	{
+		sprintf(obj_desc, "Switch %d", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_STATE, 1, 1, 0, 1, 0, MBN_DATATYPE_STATE, 1, 0, 1, 0, 0);
+	}
+	/* 1241 - 1289: CRM switches ON color */
+	for (cntSwitch=0; cntSwitch<49; cntSwitch++) 
+	{
+		sprintf(obj_desc, "Switch %d on color", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 3, 2, 2);
+	}
+	/* 1290 - 1338: CRM switches OFF color */
+	for (cntSwitch=0; cntSwitch<49; cntSwitch++) 
+	{
+		sprintf(obj_desc, "Switch %d off color", cntSwitch+1); 
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_NODATA, MBN_DATATYPE_STATE, 1, 0, 3, 0, 0);
+	}
+	/* 1339 - 1342: Potmeters, also actuator to internally save current value set by other nodes (not in sync status) */
+	for (cntSwitch=0; cntSwitch<4; cntSwitch++) 
+	{
+		sprintf(obj_desc, "Potential Meter %d", cntSwitch+1);
+		objects[cntObject++] = MBN_OBJ(obj_desc, MBN_DATATYPE_UINT, 1, 2, 0, 1023, 0, MBN_DATATYPE_UINT, 2, 0, 1023, 0, 0);
+	}	
+	
+	
+	
+	// // //------------------------------------------------------------------------------------------------
+    thisnodeB.NumberOfObjects = cntObject;
+
+
+	if((mbn = mbnInit(&thisnodeB, objects, itf, error)) == NULL)
+	{
+		return; // memLog(LF_APP, "Init failed: %s", error);
+	}
+
+    // Callbacks
+    mbnSetSensorDataChangedCallback(mbn, mSensorDataChanged);
+    //mbnSetSetActuatorDataCallback(mbn, mSetActuatorData);
+    //mbnSetErrorCallback(mbn, mError);
+    mbnSetOnlineStatusCallback(mbn, mOnlineStatus);
+
+
+	mbnStartInterface(itf, error);
+
+    lstInterfaces->Enabled = false;
+    UDPEdit->Enabled = false;
+    //TCPEdit->Enabled = false;
+    //cseUniqueID->Enabled = false;
+    btnOpen->Enabled = false;
+    //btnClose->Enabled = true;
+
+    mbnSendPingRequest(mbn, MBN_BROADCAST_ADDRESS);
+
+
+
+}
+//---------------------------------------------------------------------------
+
+int oldOnline, started;
+static int oldtst, oldtstR, newtst;
+
+void __fastcall TWillemForm1::RefreshTimerTimer(TObject *Sender)
+{
+        if ( online != oldOnline)
+        {
+			memLog->Lines->Add(online);
+            started=1;
+        }
+
+		online = oldOnline;
+
+
+		if ( started )
+		{
+
+			if (oldtst > 0)         oldtst = oldtst - 8;
+			if (oldtst < 0)         oldtst = VuValue;
+			if (oldtst <= VuValue)  oldtst = VuValue;
+
+
+
+
+
+		   if (oldtstR > 0)         oldtstR = oldtstR - 8;
+		   if (oldtstR < 0)         oldtstR = vuValueR;
+		   if (oldtstR <= VuValue)  oldtstR = vuValueR;
+
+		   ProgressBar1->Position = oldtst;
+		   ProgressBar2->Position = oldtstR;
+
+			//char* dest = new char[100];
+			char buf[100];
+//			dest[0] = 0; //Zero length string to start
+//			strcat(buf, bufferVuMeter[0]);
+//			strcat(dest, " is");
+//			strcat(dest, " oke");
+
+		   sprintf(buf,"%s %s %s", bufferVuMeter, bufferVuMeter, bufferVuMeter);
+
+
+		   //memLog->Lines->Add(bufferVuMeter[0] + ' ' + bufferVuMeter[1] + ' ' + bufferVuMeter[1]);
+
+		}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TWillemForm1::Button1Click(TObject *Sender)
+{
+	WillemForm1->RefreshTimer->Enabled = false;
+    //Timer1->Enabled=false;
+}
+//---------------------------------------------------------------------------
+
+
